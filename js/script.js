37464e7dd12ba388d6d88f5751cb86c05afe17d6
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-07-01 14:15:51 -0300

    closes #3 Script.JS : criação dos arrays

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var students = [
+    {
+        id: 1,
+        name: "Glauco",
+        email: "glauco@email.com",
+        telephone: "11973631403",
+        course: 1,
+        morning: true,
+        afternoon: false,
+        night: false,
+    },
+    {
+        id: 2,
+        name: "Luis",
+        email: "luiso@email.com",
+        telephone: "1191231333",
+        course: 2,
+        morning: false,
+        afternoon: true,
+        night: false,
+    },
+    {
+        id: 3,
+        name: "Leonardo",
+        email: "leo@email.com",
+        telephone: "119321321",
+        course: 3,
+        morning: false,
+        afternoon: false,
+        night: true,
+    },
+];
+
+var courses = [
+    { id: 1, name: "Java" },
+    { id: 2, name: "Angular" },
+    { id: 3, name: "HTML + CSS" }
+];
