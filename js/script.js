0bc0a1befdb693ff891f14570ad716ca23129a2b
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-07-01 14:37:05 -0300

    closes #4

--- js/script.js
+++ js/script.js
@@ -5,9 +5,7 @@ var students = [
         email: "glauco@email.com",
         telephone: "11973631403",
         course: 1,
-        morning: true,
-        afternoon: false,
-        night: false,
+        period: "Manhã",
     },
     {
         id: 2,
@@ -15,9 +13,7 @@ var students = [
         email: "luiso@email.com",
         telephone: "1191231333",
         course: 2,
-        morning: false,
-        afternoon: true,
-        night: false,
+        period: "Tarde",
     },
     {
         id: 3,
@@ -25,9 +21,7 @@ var students = [
         email: "leo@email.com",
         telephone: "119321321",
         course: 3,
-        morning: false,
-        afternoon: false,
-        night: true,
+        period: "Noite",
     },
 ];
 
@@ -36,3 +30,35 @@ var courses = [
     { id: 2, name: "Angular" },
     { id: 3, name: "HTML + CSS" }
 ];
+
+loadStudents();
+
+function loadStudents() {
+    for (let op of students) {
+        addNewRow(op);
+    }
+}
+
+function addNewRow(op) {
+    var table = document.getElementById("studentstable");
+
+    var newRow = table.insertRow();
+
+    var idNode = document.createTextNode(op.id);
+    newRow.insertCell().appendChild(idNode);
+
+    var nameNode = document.createTextNode(op.name);
+    newRow.insertCell().appendChild(nameNode);
+
+    var emailNode = document.createTextNode(op.email);
+    newRow.insertCell().appendChild(emailNode);
+
+    var telephoneNode = document.createTextNode(op.telephone);
+    newRow.insertCell().appendChild(telephoneNode);
+
+    var courseNode = document.createTextNode(courses[op.course - 1].name);
+    newRow.insertCell().appendChild(courseNode);
+
+    var periodNode = document.createTextNode(op.period);
+    newRow.insertCell().appendChild(periodNode);
+}
